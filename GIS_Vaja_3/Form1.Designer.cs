7956f544f392ad308c5f0123d9296bff67130b14
Kriothir <65174052+Kriothir@users.noreply.github.com>
2022-07-02 09:50:00 +0100

    Add files via upload

---
+++ GIS_Vaja_3/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿
+namespace GIS_Vaja_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(140, 116);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(535, 553);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(941, 116);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(564, 559);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click_1);
+            this.pictureBox2.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox2_Paint);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(286, 834);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1205, 834);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(226, 895);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(174, 31);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(1164, 904);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(175, 31);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(226, 949);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(174, 31);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(1164, 965);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(175, 31);
+            this.textBox4.TabIndex = 7;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(704, 875);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(174, 31);
+            this.textBox5.TabIndex = 8;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(704, 949);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(174, 31);
+            this.textBox6.TabIndex = 9;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(774, 160);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1902, 1033);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ GIS_Vaja_3/Form1.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GIS_Vaja_3
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        Pen p;
+        public Form1()
+        {
+            InitializeComponent();
+            g = this.CreateGraphics();
+            p = new Pen(Color.Blue, 2);
+        }
+        string pathImage_1 = "";
+        string pathImage_2 = "";
+
+        int counter_1 = 0;
+        int counter_2 = 0;
+
+        int image_1_x = 0;
+        int image_1_y = 0;
+
+        int image_2_x = 0;
+        int image_2_y = 0;
+        Point p1;
+        Point p2;
+
+        Point p1_1;
+        Point p2_1;
+
+        float angleImage1 = 0;
+        float angleImage2 = 0;
+        private Bitmap cutWhite(string path)
+        {
+            Bitmap source = new Bitmap(path);
+            source.MakeTransparent();
+            for (int x = 0; x < source.Width; x++)
+            {
+                for (int y = 0; y < source.Height; y++)
+                {
+                    Color currentColor = source.GetPixel(x, y);
+                    if (currentColor.R >= 255 && currentColor.G >= 255 && currentColor.B >= 255)
+                    {
+                        source.SetPixel(x, y, Color.Transparent);
+                    }
+                }
+            }
+            return source;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    pathImage_2 = openFileDialog.FileName;
+
+                    Image img = Image.FromFile(pathImage_2);
+                    pictureBox2.Width = img.Width;
+                    pictureBox2.Height = img.Height;
+
+                }
+                pictureBox2.Image = Image.FromFile(pathImage_2);
+            }
+        }
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    pathImage_1 = openFileDialog.FileName;
+
+                    //Read the contents of the file into a stream
+                    var fileStream = openFileDialog.OpenFile();
+                    Image img = Image.FromFile(pathImage_1);
+                    pictureBox1.Width = img.Width;
+                    pictureBox1.Height = img.Height;
+                    pictureBox1.Image = Image.FromFile(pathImage_1);
+
+                }
+            }
+        }
+        public static Point RotatePoints(float xRotationCenter, float yRotationCenter, float angleInDegree, int x, int y)
+        {
+            var angleInRadiant = (angleInDegree / 180.0) * Math.PI;
+
+            var cosa = (float)Math.Cos(angleInRadiant);
+            var sina = (float)Math.Sin(angleInRadiant);
+
+            // First translation
+            int t1x = (int)(x - xRotationCenter);
+            int t1y = (int)(y - yRotationCenter);
+
+            // Rotation
+            int rx = (int)(t1x * cosa - t1y * sina);
+            int ry = (int)(t1x * sina + t1y * cosa);
+
+            // seconde translation
+            x = (int)(rx + xRotationCenter);
+            y = (int)(ry + yRotationCenter);
+
+            Point newPoint = new Point((int)x, (int)y);
+
+            return newPoint;
+
+            //Console.WriteLine(x);
+           // Console.WriteLine(y);
+        }
+        private static Bitmap RotateImage(Bitmap bmp, float angle)
+        {
+            Bitmap rotatedImage = new Bitmap(bmp.Width, bmp.Height);
+            rotatedImage.SetResolution(bmp.HorizontalResolution, bmp.VerticalResolution);
+
+            using (Graphics g = Graphics.FromImage(rotatedImage))
+            {
+                // Set the rotation point to the center in the matrix
+                g.TranslateTransform(bmp.Width / 2, bmp.Height / 2);
+                // Rotate
+                g.RotateTransform(angle);
+                // Restore rotation point in the matrix
+                g.TranslateTransform(-bmp.Width / 2, -bmp.Height / 2);
+                // Draw the image on the bitmap
+                g.DrawImage(bmp, new Point(0, 0));
+            }
+
+            return rotatedImage;
+        }
+        private float getAngle(float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4)
+        {
+            float angle1 = (float)Math.Atan2(y2 - y1, x1 - x2);
+            float angle2 = (float)Math.Atan2(y4 - y3, x3 - x4);
+            Console.WriteLine("Radians" + (angle1 - angle2));
+            float calculatedAngle = (float)(180 / Math.PI) * (angle1 - angle2);
+            if (calculatedAngle < 0) calculatedAngle += 360;
+            return (360 - calculatedAngle);
+        }
+        private static void mergeImages(Image big, Image small, int x_small, int y_small, int x_big, int y_big)
+        {
+            Image img = new Bitmap(big.Width + 300, big.Height + 300);
+            using (Graphics gr = Graphics.FromImage(img))
+            {
+                gr.DrawImage(big, new Rectangle(0 + 300, 0 + 300, img.Width, img.Height), 0, 0, img.Width, img.Height, GraphicsUnit.Pixel, null);
+                gr.DrawImage(small, new Rectangle(300 + x_big - x_small, 300 + y_big - y_small, small.Width, small.Height), 0, 0, small.Width, small.Height, GraphicsUnit.Pixel, null);
+            }
+            img.Save(@"C:\Users\Admin\Desktop\Ortofoto\output33333.png", ImageFormat.Png);
+
+        }
+        int small_first_x = 0;
+        int small_first_y = 0;
+        int small_second_x = 0;
+        int small_second_y = 0;
+
+        int big_first_x = 0;
+        int big_first_y = 0;
+        int big_second_x = 0;
+        int big_second_y = 0;
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+
+            MouseEventArgs me = (MouseEventArgs)e;
+
+            if (counter_1 == 0)
+            {
+                Point coordinates = me.Location;
+                p1_1 = me.Location;
+                textBox1.Text = coordinates.X + ", " + coordinates.Y;
+                counter_1++;
+
+                small_first_x = coordinates.X;
+                small_first_y = coordinates.Y;
+
+            }
+            else if(counter_1 == 1)
+            {
+                Point coordinates = me.Location;
+                p2_1 = me.Location;
+
+                textBox3.Text = coordinates.X + ", " + coordinates.Y;
+                counter_1++;
+                textBox5.Text = Convert.ToString(CalculateDistance(p1_1, p2_1));
+
+                small_second_x = coordinates.X;
+                small_second_y = coordinates.Y;
+
+            }
+            
+        }
+
+        private void pictureBox2_Click_1(object sender, EventArgs e)
+        {
+            MouseEventArgs me = (MouseEventArgs)e;
+           
+            if (counter_2 == 0)
+            {
+                Point coordinates = me.Location;
+                p1 = me.Location;
+                image_1_x = coordinates.X;
+                image_1_y = coordinates.Y;
+
+                big_first_x = coordinates.X;
+                big_first_y = coordinates.Y;
+
+                textBox2.Text = coordinates.X + ", " + coordinates.Y;
+                counter_2++;
+
+            }
+            else if (counter_2 == 1)
+            {
+                Point coordinates = me.Location;
+                p2 = me.Location;
+
+                image_2_x = coordinates.X;
+                image_2_y = coordinates.Y;
+
+                big_second_x = coordinates.X;
+                big_second_y = coordinates.Y;
+
+                textBox4.Text = coordinates.X + ", " + coordinates.Y;
+                counter_2++;
+                textBox6.Text =  Convert.ToString(CalculateDistance(p1, p2));
+                pictureBox2.Refresh();
+            }
+
+        }
+        double CalculateDistance(Point p1, Point p2)
+        {
+            double dist = (int)Math.Sqrt(Math.Pow(p2.X - p1.X, 2) + Math.Pow(p2.Y - p1.Y, 2));
+
+            return dist;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Paint(object sender, PaintEventArgs e)
+        {
+            g.DrawLine(p, image_2_x, image_2_y, image_1_x, image_1_y);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Image big = Image.FromFile(pathImage_2);
+            Image small = Image.FromFile(pathImage_1);
+            small = cutWhite(pathImage_1);
+            float angle = getAngle(big_first_x, big_first_y, big_second_x, big_second_y, small_first_x, small_first_y, small_second_x, small_second_y);
+            small = RotateImage((Bitmap)small, angle);
+            Point newPoint = RotatePoints(small.Height / 2, small.Width / 2, angle, small_first_x, small_first_y);
+            mergeImages(big, small, newPoint.X, newPoint.Y, big_first_x, big_first_y);
+        }
+    }
+}
